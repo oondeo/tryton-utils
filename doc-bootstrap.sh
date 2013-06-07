c6969360d673af6afec3b4a25643dd7119636ec3
?ngel ?lvarez <angel@nan-tic.com>
2013-06-07 06:35:41 +0200

    add script to update trytond-doc modules, get all installed modules from a database

--- doc-bootstrap.sh
+++ doc-bootstrap.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 echo $(pwd)
+echo "Please install the last version of trydoc and sphinxcontrib-inheritance"
+echo "hg clone https://bitbucket.org/nantic/sphinxcontrib-inheritance"
+echo "hg clone https://bitbucket.org/nantic/trydoc" 
+
 pushd buildout
 ./build/bin/buildout -c userdoc.cfg
 ./create-doc-symlinks.sh
