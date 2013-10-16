ace5365c582277eee33539cfe064e272622c944b
Albert Cervera i Areny <albert@nan-tic.com>
2013-10-17 00:47:36 +0200

    Warn if new-module.sh is called without parameters.

---
+++ create-xmls.py
@@ -0,0 +1,377 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+##############################################################################
+# Copyright (C) 2013 NaN·tic
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##############################################################################
+
+import warnings
+import sys
+import os
+import optparse
+import logging
+
+
+def parse_arguments(arguments):
+    parser = optparse.OptionParser(usage='xmls-create.py [options] module')
+    parser.add_option('', '--trytond-dir', dest='trytond_dir',
+            help='set trytond directory')
+    parser.add_option('', '--stdout', action='store_true', dest='stdout',
+            help='set output to stdout', default=False)
+    parser.add_option('', '--model', dest='model',
+            help='Filter only this model')
+    parser.add_option('-v', '--verbose', action='store_true', dest='verbose',
+            help='Print more verbose log messages', default=False)
+    (option, arguments) = parser.parse_args(arguments)
+
+    # Remove first argument because it's application name
+    arguments.pop(0)
+
+    if option.trytond_dir:
+        directory = os.path.abspath(option.trytond_dir)
+    else:
+        directory = os.path.abspath(os.path.normpath(os.path.join(os.getcwd(),
+                    'trytond')))
+
+    if option.verbose:
+        logger.setLevel(logging.DEBUG)
+        logging.basicConfig(stream=sys.stderr, level=logging.ERROR)
+    else:
+        logger.setLevel(logging.INFO)
+        logging.basicConfig(stream=sys.stderr, level=logging.ERROR)
+
+    if os.path.isdir(directory):
+        sys.path.insert(0, directory)
+
+    if not arguments:
+        parser.error("module is required")
+    module_name = arguments.pop(0)
+
+    return option, module_name
+
+logger = logging.getLogger('create_xmls')
+options, module_name = parse_arguments(sys.argv)
+
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import DB_NAME, USER, CONTEXT
+from trytond.transaction import Transaction
+from trytond.pool import Pool
+
+
+def write_view_file(filename, view):
+    if options.stdout:
+        print view
+        return
+
+    if not os.path.exists('view'):
+        os.mkdir('view')
+
+    f = open(os.path.join('view', filename), 'w')
+    try:
+        f.write(view)
+    finally:
+        f.close()
+
+
+def generate_tree_view(module_name, model_name, description, inherit_type,
+        inherit, fields):
+    inherit_tag = ''
+    arch = '''\
+<?xml version="1.0"?>
+<!-- The COPYRIGHT file at the top level of this repository contains the full
+     copyright notices and license terms. -->'''
+
+    tabs = "    "
+    if inherit_type and inherit_type == 'extends':
+        inherit_tag = '\n\
+        <!-- TODO fill "ref" attribute with inherited view of model %s -->\n\
+            <field name="inherit" ref=""/>' % inherit
+        arch += '\n<data>'
+        arch += '\n<!-- TODO fill "xpath expr" -->\
+                \n    <xpath expr="" position="before">'
+        tabs += "    "
+    else:
+        arch += '\n<tree string="%s">' % description
+        if inherit:
+            arch += '\n<!-- TODO add %s model(s) fields -->' % inherit
+
+    for fieldname in fields:
+        arch += '\n%s<field name="%s"/>' % (tabs, fieldname)
+    if inherit_type and inherit_type == 'extends':
+        arch += '\n    </xpath>'
+        arch += '\n</data>'
+    else:
+        arch += '\n</tree>'
+
+    id = model_name.replace('.', '_')
+    view_file = "%s_list.xml" % id
+    output = """
+        <record model="ir.ui.view" id="%s_view_list">
+            <field name="model">%s</field>
+            <field name="type">tree</field>%s
+            <field name="name">%s_list</field>
+        </record>""" % (id, model_name, inherit_tag, id)
+    write_view_file(view_file, arch)
+    return output
+
+
+def generate_form_view(module_name, model_name, description, inherit_type,
+        inherit, fields):
+
+    inherit_tag = ''
+    arch = '''\
+<?xml version="1.0"?>
+<!-- The COPYRIGHT file at the top level of this repository contains the full
+     copyright notices and license terms. -->'''
+
+    tabs = "    "
+    if inherit_type and inherit_type == 'extends':
+        inherit_tag = '\n\
+        <!-- TODO fill "ref" attribute with inherited view of model %s -->\n\
+            <field name="inherit" ref=""/>' % inherit
+        arch += '\n<data>'
+        arch += '\n<!-- TODO fill "xpath expr" -->\
+                \n    <xpath expr="" position="before">'
+        tabs += "    "
+    else:
+        arch += '\n<form string="%s">' % description
+        if inherit:
+            arch += (
+                '\n <!-- TODO add %s model(s) fields -->'
+                % inherit)
+
+    for fieldname in fields:
+        arch += '\n%s<label name="%s"/>' % (tabs, fieldname)
+        arch += '\n%s<field name="%s"/>' % (tabs, fieldname)
+    if inherit_type and inherit_type == 'extends':
+        arch += '\n    </xpath>'
+        arch += '\n</data>'
+    else:
+        arch += '\n</form>'
+
+    id = model_name.replace('.', '_')
+    view_file = "%s_form.xml" % id
+    output = """
+        <record model="ir.ui.view" id="%s_view_form">
+            <field name="model">%s</field>
+            <field name="type">form</field>%s
+            <field name="name">%s_form</field>
+        </record>""" % (id, model_name, inherit_tag, id)
+
+    write_view_file(view_file, arch)
+    return output
+
+
+def generate_action(model_name, description):
+    id = model_name.replace('.', '_')
+    output = """
+        <record model="ir.action.act_window" id="act_%s">
+            <field name="name">%s</field>
+            <field name="res_model">%s</field>
+        </record>""" % (id, description, model_name)
+    output += """
+        <record model="ir.action.act_window.view" id="act_%s_view1">
+            <field name="sequence" eval="10"/>
+            <field name="view" ref="%s_view_list"/>
+            <field name="act_window" ref="act_%s"/>
+        </record>""" % (id, id, id)
+    output += """
+        <record model="ir.action.act_window.view" id="act_%s_view2">
+            <field name="sequence" eval="20"/>
+            <field name="view" ref="%s_view_form"/>
+            <field name="act_window" ref="act_%s"/>
+        </record>""" % (id, id, id)
+    return output
+
+
+def generate_users(module_name):
+    output = """
+        <record model="res.group" id="group_%s_admin">
+            <field name="name">%s Administration</field>
+        </record>""" % (module_name, module_name.capitalize())
+    output += """
+        <record model="res.user-res.group" id="user_admin_group_%s_admin">
+            <field name="user" ref="res.user_admin"/>
+            <field name="group" ref="group_%s_admin"/>
+        </record>""" % (module_name, module_name)
+    output += """
+        <record model="res.user-res.group" id="user_trigger_group_%s_admin">
+            <field name="user" ref="res.user_trigger"/>
+            <field name="group" ref="group_%s_admin"/>
+        </record>""" % (module_name, module_name)
+    output += """
+        <record model="res.group" id="group_%s">
+            <field name="name">%s</field>
+        </record>""" % (module_name, module_name.capitalize())
+    output += """
+        <record model="res.user-res.group" id="user_admin_group_%s">
+            <field name="user" ref="res.user_admin"/>
+            <field name="group" ref="group_%s"/>
+        </record>""" % (module_name, module_name)
+    output += """
+        <record model="res.user-res.group" id="user_trigger_group_%s">
+            <field name="user" ref="res.user_trigger"/>
+            <field name="group" ref="group_%s"/>
+        </record>""" % (module_name, module_name)
+    return output
+
+
+def generate_access(module_name, model_name):
+    id = model_name.replace('.', '_')
+    output = """
+        <record model="ir.model.access" id="access_%s">
+            <field name="model" search="[('model', '=', '%s')]"/>
+            <field name="perm_read" eval="True"/>
+            <field name="perm_write" eval="False"/>
+            <field name="perm_create" eval="False"/>
+            <field name="perm_delete" eval="False"/>
+        </record>""" % (id, model_name)
+    output += """
+        <record model="ir.model.access" id="access_%s_admin">
+            <field name="model" search="[('model', '=', '%s')]"/>
+            <field name="group" ref="group_%s_admin"/>
+            <field name="perm_read" eval="True"/>
+            <field name="perm_write" eval="True"/>
+            <field name="perm_create" eval="True"/>
+            <field name="perm_delete" eval="True"/>
+        </record>""" % (id, model_name, module_name)
+    return output
+
+
+def generate_menus(module_name, model):
+    id = model.model.replace('.', '_')
+    output = ('        <menuitem action="act_%s" id="menu_%s" parent="menu_%s"'
+        ' sequence="1" name="%s"/>\n' % (id, id, module_name, model.name))
+    return output
+
+
+def create_xml(filename, module_name, model_names):
+    output = ''
+    output += '<?xml version="1.0" encoding="utf-8"?>\n'
+    output += '<tryton>\n'
+    output += '    <data>\n'
+    menus_output = ''
+    output += generate_users(module_name)
+    with Transaction().start(DB_NAME, USER, context=CONTEXT):
+        pool = Pool()
+        Model = pool.get('ir.model')
+        Fields = pool.get('ir.model.field')
+        models = Model.search([('module', '=', module_name)])
+        for model in models:
+            if not model.model in model_names:
+                continue
+            if '-' in model.model:
+                continue
+            Class = pool.get(model.model)
+            fields = Class._fields.keys()
+            fields = sorted(list(set(fields) - set(['id', 'create_uid',
+                        'create_date', 'write_uid', 'write_date',
+                                                    'rec_name'])))
+            output += generate_form_view(module_name, model.model,
+                model.name, None, None, fields)
+            output += generate_tree_view(module_name, model.model,
+                model.name, None, None, fields)
+            output += generate_action(model.model, model.name)
+            output += generate_access(module_name, model.model)
+            menus_output += generate_menus(module_name, model)
+        fields = Fields.search([
+                ('module', '=', module_name),
+                ('model', 'not in', models),
+            ])
+        inherited_models = {}
+        for field in fields:
+            Class = pool.get(field.model.model)
+            model = field.model
+            if model in inherited_models:
+                inherited_models[model].append(field.name)
+            else:
+                inherited_models[model] = [field.name]
+        for model, fields in inherited_models.iteritems():
+            output += generate_form_view(module_name, model.model,
+                model.name, 'extends', model.model, fields)
+            output += generate_tree_view(module_name, model.model,
+                model.name, 'extends', model.model, fields)
+
+    output += '\n'
+    if menus_output:
+        output += '        <!-- Menus -->\n'
+        output += ('        <menuitem id="menu_%s" name="%s" sequence="1" />\n'
+            % (module_name, module_name.capitalize()))
+        output += '''
+          <record model="ir.ui.menu-res.group" id="menu_%s_group_%s">
+            <field name="menu" ref="menu_%s"/>
+            <field name="group" ref="group_%s"/>
+          </record>
+          ''' % (module_name, module_name, module_name, module_name)
+        output += '''
+          <record model="ir.ui.menu-res.group" id="menu_%s_group_%s_admin">
+            <field name="menu" ref="menu_%s"/>
+            <field name="group" ref="group_%s"/>
+          </record>
+          ''' % (module_name, module_name, module_name, module_name)
+        output += '\n'
+        output += menus_output
+    output += '    </data>\n'
+    output += '</tryton>'
+    return output
+
+
+def get_python_files(module_name):
+    files = {}
+    with Transaction().start(DB_NAME, USER, context=CONTEXT):
+        pool = Pool()
+        Model = pool.get('ir.model')
+        models = Model.search([('module', '=', module_name)])
+        for model in models:
+            Class = pool.get(model.model)
+            module_path = Class.__module__
+            # Expected output
+            # 'trytond.modules.<module_name>[.<subdir>].<model.model>
+            filename = module_path.split('.')[-1]
+            if not filename.startswith(module_name):
+                filename = module_name + '_' + filename
+            files.setdefault(filename, []).append(Class.__name__)
+    return files
+
+
+if __name__ == '__main__':
+    warnings.filterwarnings(action='ignore', category=DeprecationWarning)
+
+    logger.info("Generating XMLs for module '%s'" % module_name)
+    os.chdir(os.path.join('./modules/', module_name, 'tests'))
+
+    trytond.tests.test_tryton.install_module(module_name)
+    files = get_python_files(module_name)
+    logger.debug("Python files: %s" % files)
+    for filename in files:
+        models = files[filename]
+        if options.model:
+            if options.model in models:
+                models = [options.model]
+            else:
+                models = []
+        if not models:
+            continue
+        output = create_xml(filename + '.xml', module_name, models)
+        if options.stdout:
+            print output
+        else:
+            logger.info('\nWriting to %s.xml...\n' % filename)
+            f = open(filename + '.xml', 'a')
+            try:
+                f.write(output)
+            finally:
+                f.close()
---
+++ translate.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+##############################################################################
+# Copyright (C) 2013 NaN·tic
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##############################################################################
+
+import os
+import sys
+import glob
+import polib
+from optparse import OptionParser
+import uuid
+from common import Settings, check_output
+import re
+
+
+class ApertiumTranslator:
+    def __init__(self, target, source='en'):
+        self.source = source
+        self.target = target
+
+    def translate(self, text):
+        lang = '%s-%s' % (self.source, self.target)
+        translation = check_output(['apertium', '-m', lang + ".tmx",
+            '-u', lang], text)
+        return translation
+
+    def translate_po(self, filename, entries='missing'):
+        print filename
+        assert entries in ('missing', 'all'), 'entries parameter must be '\
+                '"missing" or "all"'
+
+        po = polib.pofile(filename)
+        if entries == 'missing':
+            # Includes fuzzy entries
+            entries = po.untranslated_entries
+        else:
+            po
+        for entry in po:
+            print entry.msgid, entry.msgstr
+            entry.msgstr = self.translate(entry.msgid)
+            if not 'fuzzy' in entry.flags:
+                entry.flags.append('fuzzy')
+        po.save()
+
+
+def parse_arguments(arguments):
+    usage = 'translate.py  -m <module> -l <lang>'
+    parser = OptionParser(usage=usage)
+    parser.add_option('-g', '--generate-tmx', dest='tmx', action="store_true",
+        default=False)
+    parser.add_option('-m', '--module', dest='module')
+    parser.add_option('-l', '--lang', dest='lang')
+
+    (option, arguments) = parser.parse_args(arguments)
+
+    settings = Settings()
+
+    if option.module and option.lang:
+        settings.module = option.module
+        settings.lang = option.lang
+    else:
+        print usage
+
+    settings.tmx = False
+    if option.tmx:
+        settings.tmx = True
+
+    return settings
+
+
+def make_translation_memory(lang):
+
+    locale_dir = os.path.join(os.getcwd(), 'modules')
+    glob_path = os.path.join(locale_dir, '*', 'locale', lang + '.po')
+
+    dst_file = os.path.join('/tmp', str(uuid.uuid4())) + ".po"
+    entries = []
+    po = polib.POFile()
+    po.metadata = {
+            'Project-Id-Version': '1.0',
+            'Report-Msgid-Bugs-To': 'angel@nan-tic.com',
+            'Language-Team': 'English <angel@nan-tic.com.com>',
+            'MIME-Version': '1.0',
+            'Content-Type': 'text/plain; charset=utf-8',
+            'Content-Transfer-Encoding': '8bit',
+    }
+
+    for src_file in glob.glob(glob_path):
+        pot = polib.pofile(src_file)
+        entries += pot.translated_entries()
+        entries += pot.fuzzy_entries()
+        entries += pot.obsolete_entries()
+
+    terms = []
+    for e in entries:
+        if e.msgid == e.msgstr:
+            continue
+        keys = re.findall('\%\(\w+\)s',e.msgid)
+        for key in keys:
+            po.append(polib.POEntry(
+                msgid=key,
+                msgstr=key))
+
+        term = (e.msgid, e.msgstr)
+        if term not in terms:
+            terms.append(term)
+
+        if e.flags:
+            e.flags.remove('fuzzy')
+        po.append(e)
+    po.save(dst_file)
+
+    check_output(['po2tmx', '-l', lang[:2], '-i', dst_file, '-o',
+         'en-%s.tmx' % lang[:2]])
+
+if __name__ == "__main__":
+
+    settings = parse_arguments(sys.argv[1:])
+
+    if settings.tmx:
+        print "* Generating tmx memmory file..."
+        make_translation_memory(settings.lang)
+        print "* Finish tmx"
+
+    print "* Start translation module:", settings.module
+    locale_dir = os.path.join(os.getcwd(), 'modules', settings.module,
+        'locale')
+    locale_file = os.path.join(locale_dir, settings.lang + '.po')
+    ap = ApertiumTranslator(settings.lang.split('_')[0])
+    ap.translate_po(locale_file)
+    print "* Translation finished"
+    print "* Please, Remember to upadte to module to update terms"
