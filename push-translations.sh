0c24379783ba1916ca1f35a647f92d1007b3b10c
Albert Cervera i Areny <albert@nan-tic.com>
2014-04-18 00:03:50 +0200

    Update push-translations.sh script.

--- push-translations.sh
+++ push-translations.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
-lang=ca_ES
-user=albertca
+user=$1
+lang=$2
+force=$3
+
+if [[ -z "$1" || -z "$2" ]]; then
+    echo "Usage: $0 user lang [action]"
+    echo
+    echo "If action is empty it will just print the commit and push commands"
+    echo "it would execute."
+    echo "If action is 'commit' it will make the commit."
+    echo "If action is 'push' it will push the commits to the server."
+    exit 0
+fi
 
 pushd trytond || exit 1
-for module in $(hg nstatus | grep "\[" | sed 's/\[//' | sed 's/\]//'); do
-    if [[ "$module" == "." ]]; then
+for directory in $(hg nstatus | grep "\[" | sed 's/\[//' | sed 's/\]//'); do
+    if [[ "$directory" == "." ]]; then
         continue
     fi
-    pushd $dir || exit 1
+    pushd $directory || exit 1
+    module=$(basename $directory)
     hg add locale/${lang}.po
-    echo hg commit -m "Update ${lang} translation"
-    #hg commit -m "Update ${lang} translation"
-    echo "hg push ssh://$user@hg.tryton.org///home/hg/modules/$module"
-    #hg push ssh://$user@hg.tryton.org///home/hg/modules/$module
+    if [[ "$force" == "commit" ]]; then
+        hg commit -m "Update ${lang} translation"
+    elif [[ "$force" == "push" ]]; then
+        hg push ssh://$user@hg.tryton.org///home/hg/modules/$module
+    else
+        echo "hg commit -m 'Update ${lang} translation'"
+        echo "hg push ssh://$user@hg.tryton.org///home/hg/modules/$module"
+    fi
     popd
 done
 
